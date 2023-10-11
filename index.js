6bdf9f0a7a9930735ae2afa521b8906d9d31c512
sylvan-li <76680992+sylvan-li@users.noreply.github.com>
2023-10-10 22:50:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -50,11 +50,7 @@ expect arguments of concatOdds to be arrays */
 
 //#3 
 /*A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
-Think about the following; you may need to make assumptions or decisions about the prompt and how the feature should behave:
-What should happen if the cart is empty?
-What needs to be shown to the user at each step of check out?
-What behaviors of this feature does the prompt miss or gloss over?
-Hint: Observe the shopping cart and checkout features of some popular websites to get some ideas!
+
 
 User adds item to cart 
 - user clicks checkout feature
